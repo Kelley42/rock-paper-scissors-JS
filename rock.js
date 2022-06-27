e3718fc97966e60a59cfd5bb89c14d87b6336141
Kelley42 <kelleykristine42@yahoo.com>
2022-06-27 18:27:56 -0500

    Add getWord func, edit compPlay func, add variables

--- rock.js
+++ rock.js
@@ -1,16 +1,30 @@
-/*function computerPlay randomly return rock, paper or scissors for computer's turn*/
+/*randomly return rock, paper or scissors for computer's turn*/
 function computerPlay(max) {
-    return Math.floor(Math.random() * max)
+    return getWord(Math.floor(Math.random() * max))
 }
-console.log(computerPlay(3))
 
-/*function playRound - to play single round of game*/
-/* 2 parameters - playerSelection, computerSelection*/
-/*return winner of round as string: "You lose! Paper beats Rock"*/
+function getWord(computerSelection) {
+    switch(computerSelection) {
+        case "rock":
+            return 0;
+        case "paper":
+            return 1;
+        case "scissors":
+            return 2;
+    }
+}
 
+/*play single round of game, return winner of round as string: "You lose! Paper beats Rock"*/
 /*playerSelection parameter case-insensitive*/
+/*function playRound(playerSelection, computerSelection) {
+    if (playerSelection > computerSelection) {
+         pass
+    }
+}*/
 
-
+/*get input from user*/
+let playerSelection = prompt("Rock, paper, or scissors?" ).toLowerCase();
+let computerSelection = computerPlay(3);
+let score = 0;
 /*function game() - call playRound function, plays 5 rounds
-/*keeps score, reports winner or loser at end
-prompt() - get input from user*/
+/*keeps score, reports winner or loser at end*/
