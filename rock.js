ef365718b62700a79290c154d5c57d4829a057d9
Kelley42 <kelleykristine42@yahoo.com>
2022-07-04 13:36:16 -0500

    Fixed playRound function, removed unnecessary game function

--- rock.js
+++ rock.js
@@ -17,90 +17,71 @@ function getWord(computerSelection) {
 
 /*play single round of game, return winner of round as string: "You lose! Paper beats Rock"*/
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.target.id
-    console.log(playerSelection)
-    computerSelection = computerPlay(3)
     let message = ""
+    playerSelection = playerSelection.target.id
+    computerSelection = computerPlay(3)
     if (playerSelection == "rock") {
-         if (computerSelection == "paper") {
-            message = "You lose! Paper beats rock."
-            computerScore += 1
-         }
-         else if (computerSelection == "scissors") {
-             message = "You win!"
-             playerScore += 1
-         }
-         else {
-             message = "Tie!"
-         }
+            if (computerSelection == "paper") {
+                message = "You lose! Paper beats rock."
+                computerScore += 1
+            }
+            else if (computerSelection == "scissors") {
+                message = "You win! Rocks beats scissors."
+                playerScore += 1
+            }
+            else {
+                message = "Tie!"
+            }
     }
     else if (playerSelection == "paper") {
-        if (computerSelection == "scissors") {
-           message = "You lose! Scissors beats paper." 
-           computerScore += 1
-        }
-        else if (computerSelection == "rock") {
-            message = "You win!" 
-            playerScore += 1
-        }
-        else {
-            message = "Tie!" 
-        }
-   }
-    else if (playerSelection == "scissors") {
-        if (computerSelection == "rock") {
-            message = "You lose! Rock beats scissors."
+            if (computerSelection == "scissors") {
+            message = "You lose! Scissors beats paper." 
             computerScore += 1
-        }
-        else if (computerSelection == "paper") {
-            message = "You win!" 
-            playerScore += 1
-        }
-        else {
-            message = "Tie!"
-        }
+            }
+            else if (computerSelection == "rock") {
+                message = "You win! Paper beats rock." 
+                playerScore += 1
+            }
+            else {
+                message = "Tie!" 
+            }
+    }
+    else if (playerSelection == "scissors") {
+            if (computerSelection == "rock") {
+                message = "You lose! Rock beats scissors."
+                computerScore += 1
+            }
+            else if (computerSelection == "paper") {
+                message = "You win! Scissors beats paper." 
+                playerScore += 1
+            }
+            else {
+                message = "Tie!"
+            }
     }
     else {
-        message = "Incorrect input"
+            message = "Incorrect input"
     }
+    // Display stats to player
     document.getElementById("round-results").innerText = `Computer played: ${computerSelection}
     Your score: ${playerScore}
     Computer score: ${computerScore}
     ${message}`;
-}
-
-/*calls playRound function 5 times, keeps score, returns winner/loser*/
-function game() {  
-    for (let i = 0; i <= 5; i++) {
-        playRound(playerSelection, computerSelection)
+    // End game after first to score 5 pts
+    if (playerScore == 5 || computerScore == 5) {
+        printWinner(computerScore, playerScore)
     }
-    console.log(computerScore, playerScore)
-    printWinner(computerScore, playerScore)
-    
-
-    // Append winner
-    /*
-    let results = document.createElement("div")
-    results.id = "results"
-    let winnerText = document.createTextNode(`Winner: ${winner}`)
-    results.appendChild(winnerText)
-    const winnerGame = document.querySelector("div #results")
-    const roundWinner = document.querySelector("div #round-results")
-    winnerGame.insertBefore(results, roundWinner)*/
-  
 }
 
+// Displays winner and makes buttons unclickable
 function printWinner(computerScore, playerScore) {
     let winner = (computerScore > playerScore) ? "You win!" : "You lose!"
     console.log(`${winner} Score: ${playerScore}`)
     document.getElementById("winner-game").textContent = winner
+    const buttons = document.querySelectorAll(".btn");
+    buttons.forEach(button => button.disabled = true);
 }
 
-
-
-
-//let rockButton = document.querySelector("#rock").addEventListener("click", playRound);
-//let paperButton = document.querySelector("#paper").addEventListener("click", playRound)
 
 let playerSelection;
 let computerSelection;
@@ -108,10 +89,7 @@ let computerScore = 0;
 let playerScore = 0;
 const buttons = document.querySelectorAll(".btn");
 buttons.forEach(button => button.addEventListener("click", playRound));
-// function buttonClick(e) {
-//     playerSelection = e.target.id
-//     playRound
-// }
+
 
 
 
